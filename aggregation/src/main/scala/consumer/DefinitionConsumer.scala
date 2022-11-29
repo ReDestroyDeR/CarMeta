b7e41179cbf856c4821fa058b7e38544bbcbf3bb
ReDestroyDeR <daniil.schroder@gmail.com>
2022-11-29 21:17:42 +0300

    Add templates

---
+++ aggregation/src/main/scala/consumer/DefinitionConsumer.scala
@@ -0,0 +1,6 @@
+package ru.red.car_meta.aggregation
+package consumer
+
+object DefinitionConsumer {
+
+}
---
+++ aggregation/src/main/scala/domain/ElasticReference.scala
@@ -0,0 +1,4 @@
+package ru.red.car_meta.aggregation
+package domain
+
+case class ElasticReference(id: String, model: String)
